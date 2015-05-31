b661b195905cd520d8a1cbc9562622e8aceee8ee
Randy Leighton <randyleighton@yahoo.com>
2015-05-31 16:31:31 -0700

    lowercase fix for the title case script

--- projects/titleCaseSentence.js
+++ projects/titleCaseSentence.js
@@ -1,6 +1,5 @@
 function titleCase(str) {
-  var titleWords = str.split(" ");
-  console.log("Titleword array: " + titleWords);
+  var titleWords = str.toLowerCase().split(" ");
   var quickWord = "";
   var finalWords = [];
   /* loop through the words array for each word */
@@ -9,9 +8,9 @@ function titleCase(str) {
     console.log(quickWord);
     finalWords.push(quickWord.charAt(0).toUpperCase() + quickWord.substring(1)); 
   }
-  return finalWords;
+  return finalWords.join(" ");
 }
-titleCase("I'm a little tea pot for the ages of time");
+titleCase("I'm a little TEA pOT for the ages of time");
 
 /* Return the provided string with the first letter of each word capitalized.
 For the purpose of this exercise, you should also 
