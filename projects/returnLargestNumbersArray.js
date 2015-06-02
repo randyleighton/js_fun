f96300706e39aa3af2fcbe86c4ef28439b200842
Randy Leighton <randyleighton@yahoo.com>
2015-06-01 19:15:11 -0700

    that function was a bear, sort 4 arrays by largest number to a single array

---
+++ projects/returnLargestNumbersArray.js
@@ -0,0 +1,19 @@
+function largestOfFour(arr) {
+  largestNums = [];
+  var tempVal = "";
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].sort().reverse();
+    arr[i].sort(function(a, b) {
+      return a - b;
+    });
+    tempVal = arr[i].slice(0,1).join();
+    largestNums.push(parseInt(tempVal));
+  }
+  return largestNums;
+}
+largestOfFour([
+              [4, 5, 1, 3], 
+              [13, 27, 18, 26], 
+              [32, 35, 37, 39], 
+              [1000, 1001, 857, 1]
+              ]);
