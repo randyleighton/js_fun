24d2003fc5aa790de07477516ae6e743ac361271
Randy Leighton <randyleighton@yahoo.com>
2015-06-02 19:43:06 -0700

    fix string save

--- projects/stringRepeat.js
+++ projects/stringRepeat.js
@@ -1,6 +1,13 @@
 function repeat(str, num) {
-
-  return str;
+newstr=""
+if (num >= 1) {
+  console.log("number +")
+  for (var i = 0 ; i < num ; i++) {
+    newstr = newstr + str
+    console.log("new string: " + newstr)
+  } 
 }
 
+  return newstr;
+}
 repeat('abc', 3);
